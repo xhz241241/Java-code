c71abbcd46933a62bf5d3a1503e5e430a3c5480e
xhz241241 <1063251507@qq.com>
2021-02-20 17:03:42 +0800

    14

--- Day14.java
+++ Day14.java
@@ -1,10 +1,42 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Day14 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
-        System.out.println(subsets(nums));
+//        int[] nums = {1, 2, 3};
+//        System.out.println(subsets(nums));
+
+        String[] tokens = {"2", "1", "+", "3", "*"};
+        System.out.println(evalRPN(tokens));
+        int f[][] = new int[2][];
+    }
+
+    public static int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0; i < tokens.length; i++){
+            switch (tokens[i]){
+                case "+" :
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-" :
+                    int a = stack.pop();
+                    stack.push(stack.pop() - a);
+                    break;
+                case "*" :
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/" :
+                    int b = stack.pop();
+                    stack.push(stack.pop() / b);
+                    break;
+                default :
+                    stack.push(Integer.parseInt(tokens[i]));
+                    break;
+            }
+        }
+        return stack.pop();
     }
 
     private static List<List<Integer>> subsets(int[] nums) {
