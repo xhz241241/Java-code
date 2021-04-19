44faa481dc054c034c09c55ce0e8533de369eb5c
xhz241241 <1063251507@qq.com>
2021-04-19 19:58:33 +0800

    每日一题 day5

--- Test_0416/Main2.java
+++ Test_0416/Main2.java
@@ -3,6 +3,7 @@ package Test_0416;
 import java.util.*;
 
 public class Main2{
+    //这道题使用了动态规划的思想
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
@@ -11,15 +12,18 @@ public class Main2{
             for(int i = 0; i < n; i++){
                 arr[i] = sc.nextInt();
             }
-            int curMax = arr[0];
-            int Max = arr[0];
+            int cur = arr[0];
+            int max = arr[0];
+            //遍历当前数组, 如果加上arr[i]比arr[i]要大的话就把arr[i]加上
+            //如果加上arr[i]比arr[i]要小的话就从arr[i]开始
+            //核心思想就是dp[i] = getmax(dp[i-1] + arr[i], arr[i])
             for(int i = 1; i < n; i++){
-                curMax = Math.max(curMax+arr[i], arr[i]);
-                if(curMax > Max){
-                    Max = curMax;
+                cur = Math.max(cur+arr[i], arr[i]);
+                if(cur > max){
+                    max = cur;
                 }
             }
-            System.out.println(Max);
+            System.out.println(max);
         }
     }
 }
