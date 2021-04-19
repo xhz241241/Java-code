d8dafba0eed28a9ca3fe864087f9debe61af5da9
xhz241241 <1063251507@qq.com>
2021-04-19 17:05:08 +0800

    每日一题 day4

--- Test_0415/Main.java
+++ Test_0415/Main.java
@@ -1,22 +1,28 @@
 package Test_0415;
 //计算糖果
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
             int n1 = sc.nextInt();
             int n2 = sc.nextInt();
             int n3 = sc.nextInt();
             int n4 = sc.nextInt();
-            int A = (n1+n3) / 2;
-            int B = (n3-n1) / 2;
-            int C = (n4-n2) / 2;
-            if((A-B == n1) && (B-C == n2) && (A+B == n3) && (B+C == n4)){
-                System.out.println(A + " " + B + " " + C);
-            }else{
+            //使用数学公式计算出ABC的值的表达式
+            //其中B有两个表达式
+            //我们只需要判断两个表达式中的B 是否相等
+            //相等代表计算正确 不等代表不存在这样的数
+            int A = (n1+n3)/2;
+            int B1 = (n3-n1)/2;
+            int B2 = (n4+n2)/2;
+            int C = (n4-n2)/2;
+
+            if(B1 != B2){
                 System.out.println("No");
+            }else{
+                System.out.println(A+" " + B1 + " " + C);
             }
         }
     }
--- Test_0415/Main2.java
+++ Test_0415/Main2.java
@@ -1,54 +1,29 @@
 package Test_0415;
+//进制转化
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Stack;
-
-public class Main2 {
-    public static void main(String[] args) {
+import java.util.*;
+public class Main2{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
             int m = sc.nextInt();
             int n = sc.nextInt();
-            if(m > 0 && n > 0){
-                my_convert(m, n);
+            StringBuilder stringbuilder = new StringBuilder();
+            String table = "0123456789ABCDEF";
+            boolean flag = false;
+            if(m < 0){
+                m = -m;
+                flag = true;
             }
-        }
-    }
-
-    private static void my_convert(int m, int n) {
-        if(n == 10){
-            System.out.println(m);
-            return;
-        }
-        //创建一个map存放16进制的字母
-        Map<Integer, String> map = new HashMap<>();
-        map.put(10, "A");
-        map.put(11, "B");
-        map.put(12, "C");
-        map.put(13, "D");
-        map.put(14, "E");
-        map.put(15, "F");
-        //创建一个栈 存放余数
-        //如果是转成16进制的话栈中需要存放数字+字符
-        //所以栈不标志类型
-        Stack stack = new Stack<>();
-        while(m > 0){
-            int yushu = m % n;
-            if(n < 10){
-                stack.push(yushu);
-            }else{
-                if(yushu > 9){
-                    stack.push(map.get(yushu));
-                }else{
-                    stack.push(yushu);
-                }
+            while(m != 0){
+                stringbuilder.append(table.charAt(m%n));
+                m = m / n;
             }
-            m = m / n;
-        }
-        while (!stack.isEmpty()){
-                System.out.print(stack.pop());
+            if(flag){
+                stringbuilder.append("-");
+            }
+            stringbuilder.reverse();
+            System.out.println(stringbuilder);
         }
     }
 }
