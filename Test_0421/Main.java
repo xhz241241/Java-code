f405b9f2c87566eadeb7029b61af61b952352a02
xhz241241 <1063251507@qq.com>
2021-04-21 11:37:16 +0800

    每日一题 day9

---
+++ Test_0421/Main.java
@@ -0,0 +1,23 @@
+package Test_0421;
+//末尾0的个数
+
+import java.util.*;
+//因为 0 是靠 乘 5 乘出来的, 所以我们只需要看输入进来的数字里面有几个 因数5 即可
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int n = sc.nextInt();
+            int count = 0;
+            for(int i = n; i >= 1; i--){
+                int tem = i;
+                while(tem % 5 == 0){
+                    count++;
+                    tem = tem / 5;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
