3ad70570cf19166250fcb36994ab8ded7b775a48
xhz241241 <1063251507@qq.com>
2021-04-26 17:58:22 +0800

    每日一题 day11

--- Test_0423/Main2.java
+++ Test_0423/Main2.java
@@ -1,4 +1,19 @@
 package Test_0423;
+import java.util.*;
+//求最近公共祖先 序号从 1 开始
 
 public class Main2 {
+    public int getLCA(int a, int b) {
+        //当 a 和 b 不相等时, 就让大的一方除以 2
+        //直到两个数相等
+        //相等的值就是他俩的公共祖先
+        while(a != b){
+            if(a > b){
+                a = a / 2;
+            }else{
+                b = b / 2;
+            }
+        }
+        return a;
+    }
 }
