cf645e87e171f8d7c91c9fe14a19ebe5eb532d21
xhz241241 <1063251507@qq.com>
2021-04-27 22:07:24 +0800

    每日一题 day12

--- Test_0424/Main.java
+++ Test_0424/Main.java
@@ -4,8 +4,10 @@ package Test_0424;
 public class Main {
     public class BinInsert {
         public int binInsert(int n, int m, int j, int i) {
+            //右移 j 位是保证 m 能够对齐在i和j之间
             m = m << j;
-            return n + m;
+            //然后之间把 n 和 m 进行或操作
+            return n | m;
         }
     }
 }
