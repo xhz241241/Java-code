7f41d9fcbae9209a29a5d6677efe266b3be0ba55
xhz241241 <1063251507@qq.com>
2021-05-14 16:57:47 +0800

    每日一题 day20

--- Test_0511/Main.java
+++ Test_0511/Main.java
@@ -18,3 +18,27 @@ public class Main{
         }
     }
 }
+
+// 2. 双指针解法
+//
+//import java.util.*;
+//
+//public class Main{
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner (System.in);
+//        while(sc.hasNext()){
+//            String s = sc.nextLine();
+//            int left = 0;
+//            int right = s.length()-1;
+//            char[] arr = s.toCharArray();
+//            while(left < right){
+//                char tem = arr[left];
+//                arr[left] = arr[right];
+//                arr[right] = tem;
+//                left++;
+//                right--;
+//            }
+//            System.out.println(arr);
+//        }
+//    }
+//}
